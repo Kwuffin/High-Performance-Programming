96d707d94bd5800e5d9b09e5c66b6b922fe63ad0
Kwuffin <bestboykevin.bbg@gmail.com>
2021-03-01 19:51:23 +0100

    Now works for uneven list lengths

--- Opdracht 3/opdracht_3.py
+++ Opdracht 3/opdracht_3.py
@@ -32,33 +32,46 @@ def compare_lists(list_a, list_b):
 
 def merge_sort(lst):
     """
-    Creates the pools.
+    Creates the pools and isolates elements on uneven lists.
 
     :param lst: Random list
     :return: Sorted list
     """
-    # if len(lst) % 2 == 0:
+
     split_elements = []
     for i in lst:
         split_elements.append([i])
 
-    print(split_elements)
-
     with concurrent.futures.ProcessPoolExecutor() as executor:
         while len(split_elements) != 1:
+
+            uneven = False
+            if len(split_elements) % 2 != 0:
+                uneven = True
+                left_over = split_elements[-1]
+                split_elements.pop(-1)
+
             results = executor.map(compare_lists, split_elements[::2], split_elements[1::2])
 
             split_elements = []
             for result in results:
                 split_elements.append(result)
 
-    return split_elements[0]
+            if uneven:
+                uneven_list = compare_lists(split_elements[0], left_over)
+                split_elements.pop(0)
+                split_elements.append(uneven_list)
+                uneven = False
+
+        return split_elements[0]
 
 
 def main():
-    randlist = np.random.randint(100, size=100000)
+    randlist = np.random.randint(100, size=100)
+    randlist = list(randlist)
+    print(f"Random list: {randlist}")
     sorted_list = merge_sort(randlist)
-    print(sorted_list)
+    print(f"Sorted list: {sorted_list}")
 
 
 if __name__ == '__main__':
